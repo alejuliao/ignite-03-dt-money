363cf79208d0495963e7b474f48af22bd0ffe385
alejuliao <alexandrejuliao@icloud.com>
2022-10-12 13:31:57 -0300

    start modal

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
   return(
     <div>
       <Header/>
       <Summary/>
+
+        <TransactionsContainer>
+          <SearchForm/>
+      <TransactionsTable>
+
+        <thead></thead>
+        <tbody>
+          <tr>
+          <td width="50%">Desenvolvimento web</td>
+          <td><PriceHighlight variant="income">12000</PriceHighlight></td>
+          <td> Venda</td>
+          <td> data</td>
+          </tr>
+          <tr>
+          <td width="50%">Desenvolvimento web</td>
+          <td><PriceHighlight variant="outcome">- 6000</PriceHighlight></td>
+          <td> Venda</td>
+          <td> data</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+        </TransactionsContainer>
     </div>
   )
 }
